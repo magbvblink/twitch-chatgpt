cc50bd9fe30839ff240e29c1c775a144bc21adce
magbvblink <152082327+magbvblink@users.noreply.github.com>
2023-11-26 17:10:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.json({extended: true, limit: '1mb'}))
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Funcionou!')
 })
 
 if (process.env.GPT_MODE === "CHAT"){
